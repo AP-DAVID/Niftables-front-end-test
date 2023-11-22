da2ef9b31171d832558782bc3bd661c8a949d172
AP-DAVID <akintolaapre@gmail.com>
2023-11-22 12:10:43 +0100

    styling done

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Profiting from '@/features/Profiting';
 import '@mantine/carousel/styles.css';
 export default function Home() {
   return (
-    <main className='bg-black h-screen overflow-y-scroll pb-20'>
+    <main className='bg-black h-screen overflow-y-scroll pb-20 sm:pb-10'>
       {/* hero */}
       <section id='hero'>
         <Hero />
